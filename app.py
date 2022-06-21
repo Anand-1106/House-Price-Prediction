0e2afe421ba2ccd7e434a8e6e90fab41fc5e88bc
Anand-1106 <97014562+Anand-1106@users.noreply.github.com>
2022-06-22 00:49:29 +0530

    updated files

--- app.py
+++ app.py
@@ -8,23 +8,23 @@ print('model loaded')
 
 X_cols = pickle.load(open('col.pkl', 'rb'))
 
-print("cols loded")
+print("cols loaded")
 
 def predict_price(location,sqft,bath,bhk):    
     loc_index = np.where(X_cols==location)[0][0]
-
     x = np.zeros(len(X_cols))
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
     if loc_index >= 0:
         x[loc_index] = 1
-
     return loaded_model.predict([x])[0]
 
 
 
 app = Flask(__name__,template_folder='templates')
+
+
 
 @app.route("/")
 def hello_world():
@@ -36,13 +36,13 @@ def predict():
     location = request.form.get('location')
     area = request.form.get('flat_area')
     bath = request.form.get('bath')
-    print(location, bhk, bath, area)
-    try:
-        res = predict_price(location, area, bath, bhk)
-        return render_template('first.html', prediction_text = f"This is ouput: {res}")
-    except:
-        return "location is not available"
+    # print(location, bhk, bath, area)
+    res = predict_price(location, area, bath, bhk)
+    return render_template("first.html", prediction_text = f"The predicted price is: {round(res, 3)} Lakhs.")
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    # localhost:5000/predict
